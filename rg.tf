da3f8f0dcbecc5481d57070816322bcde4d8c1dd
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2021-09-28 19:17:57 -0400

    Create rg.tf

---
+++ rg.tf
@@ -0,0 +1,14 @@
+module "resource-group" {
+  source  = "app.terraform.io/Pooja1892/resource-group/azurerm"
+  version = "1.0.0"
+  rg-name = var.prov-rg-name
+  rg-location = "eastus2"
+  # insert required variables here
+}
+  module "resource-group" {
+  source  = "app.terraform.io/Pooja1892/resource-group/azurerm"
+  version = "1.0.1"
+  rg-name = var.prov-rg-name
+  rg-location = "centralus"
+  # insert required variables here
+}
