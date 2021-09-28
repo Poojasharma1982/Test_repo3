03b377b7989bf2b91cd2bc8428c3a5ddf1b98eca
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2021-09-28 19:51:15 -0400

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -1,11 +1,11 @@
-module "resource-group" {
+module "resource-group-eastus" {
   source  = "app.terraform.io/Pooja1892/resource-group/azurerm"
   version = "1.0.0"
   rg-name = var.prov-rg-name
   rg-location = "eastus2"
   # insert required variables here
 }
-  module "resource-group" {
+  module "resource-group-centralus" {
   source  = "app.terraform.io/Pooja1892/resource-group/azurerm"
   version = "1.0.1"
   rg-name = var.prov-rg-name
