35ea7eaaf2b4dcc50a168f6642315637e7b465ca
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2021-09-28 22:44:40 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,12 +1,3 @@
-variable "prov-rg-name" {
-  type = string
-  # default = "01000000-0000-4000-8000-000030080200"
-}
-
-variable "prov-rg-location" {
-  type = string
-  # default = "01000000-0000-4000-8000-000030080200"
-}
 variable "prov-rg-eastus2-name" {
   type = string
   # default = "01000000-0000-4000-8000-000030080200"
