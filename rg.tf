5e68c56c6b4fab10c8c5797ccdc2f947ab48b0d8
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2021-09-28 22:29:59 -0400

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -12,3 +12,10 @@ module "resource-group-new" {
  # rg-location = "centralus"
   # insert required variables here
 # }
+  module "resource-rg-module" {
+  source  = "app.terraform.io/Pooja1892/resource-rg-module/azurerm"
+  version = "1.0.4"
+    rg-eastus2-name = var.prov-rg-eastus2-name
+    rg-centralus-name = var.prov-rg-centralus-name
+  # insert required variables here
+}
