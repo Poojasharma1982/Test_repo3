a8616ff8815c5e2fd1cb7ebfc7062c42be1c674b
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2021-09-30 21:10:41 -0400

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -22,7 +22,7 @@ module "vnet-module" {
   
   module "subnet" {
   source  = "app.terraform.io/Pooja1892/subnet/azurerm"
-  version = "1.0.1" 
+  version = "1.0.3" 
   vnet_name_new =var.prov-vnet_name
   vnet_address_new = var.prov-vnet_address
   rg_name_new = var.prov-rg-eastus2-name
