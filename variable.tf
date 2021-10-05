e8d2adaf59a92053ca8a83e74fe59acc6ad61a7a
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2021-10-04 23:12:23 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -28,7 +28,3 @@ variable "prov-networksecuritygroup-name" {
   type = string
   # default = "01000000-0000-4000-8000-000030080200"
 }
-variable "prov-subnet-id" {
-  type = string
-  # default = "01000000-0000-4000-8000-000030080200"
-}
