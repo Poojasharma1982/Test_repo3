bd1af597bb4451178919327c70afdbcc8812e733
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2021-10-06 21:58:06 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -32,3 +32,11 @@ variable "prov-routetable-name" {
   type = string
   # default = "01000000-0000-4000-8000-000030080200"
 }
+variable "prov-route-name" {
+  type = string
+  # default = "01000000-0000-4000-8000-000030080200"
+}
+variable "prov-route-table-name" {
+  type = string
+  # default = "01000000-0000-4000-8000-000030080200"
+}
