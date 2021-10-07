1799534fce079999c56fbe8d0056b853d87a7c9f
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2021-10-06 21:59:51 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -36,7 +36,7 @@ variable "prov-route-name" {
   type = string
   # default = "01000000-0000-4000-8000-000030080200"
 }
-variable "prov-route-table-name" {
+variable "prov-rt-nsg-name" {
   type = string
   # default = "01000000-0000-4000-8000-000030080200"
 }
